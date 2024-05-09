4a40c5281d9d7c87737f410482d339dbd132318f
JEON-Sungsu <anagma@naver.com>
2024-05-09 22:47:27 +0900

    feat: 위치권한 요청 메서드 추가

--- lib/common/presentation/splash_screen.dart
+++ lib/common/presentation/splash_screen.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:leave_subway/common/const/color.dart';
 import 'package:lottie/lottie.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,5 +35,29 @@ class SplashScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled =
+    await Geolocator.isLocationServiceEnabled(); //위치정보 서비스가 이용가능한 기기인지 체크
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission(); //권한 획득했는지 확인
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission(); //권한 요청하기
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
   }
 }

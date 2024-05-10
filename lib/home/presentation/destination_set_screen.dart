cb594354dbbc0e43cbd6d37b3e0fb439dda055a0
JEON-Sungsu <anagma@naver.com>
2024-05-10 18:15:04 +0900

    feat: 메인화면 진입시, 위치 권한에 따른 얼럿띄우기 처리

--- lib/home/presentation/destination_set_screen.dart
+++ lib/home/presentation/destination_set_screen.dart
@@ -1,10 +1,76 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class DestinationSetScreen extends StatelessWidget {
-  const DestinationSetScreen({super.key});
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:leave_subway/common/component/cupertino_dialog.dart';
+import 'package:leave_subway/common/component/material_dialog.dart';
+
+class DestinationSetScreen extends StatefulWidget {
+  final bool _serviceEnabled;
+  final LocationPermission _permission;
+
+  const DestinationSetScreen({
+    super.key,
+    required bool serviceEnabled,
+    required LocationPermission permission,
+  })  : _serviceEnabled = serviceEnabled,
+        _permission = permission;
+
+  @override
+  State<DestinationSetScreen> createState() => _DestinationSetScreenState();
+}
+
+class _DestinationSetScreenState extends State<DestinationSetScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      _showAlert();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text('하이'),
+      ),
+    );
+  }
+
+  void _showAlert() {
+    if (widget._permission == LocationPermission.denied ||
+        widget._permission == LocationPermission.deniedForever) {
+      Platform.isIOS
+          ? showCupertinoModalPopup(
+          context: context,
+          builder: (BuildContext context) {
+            return CupertinoDialog(
+              title: '알림',
+              content: '기기 위치에 접근 권한을 허용해야 원활한 서비스 이용이 가능합니다.',
+              hasCancelButton: false,
+              actionButtonTitle: '확인',
+              onPressAction: () {
+                Navigator.pop(context, 'Cancel');
+              },
+            );
+          })
+          : showDialog(
+        context: context,
+        builder: (context) {
+          return MaterialDialog(
+            title: '알림',
+            content: '기기 위치에 접근 권한을 허용해야 원활한 서비스 이용이 가능합니다.',
+            hasCancelButton: false,
+            actionButtonTitle: '확인',
+            onPressAction: () {
+              Navigator.pop(context, 'Cancel');
+            },
+          );
+        },
+      );
+    }
   }
 }
+

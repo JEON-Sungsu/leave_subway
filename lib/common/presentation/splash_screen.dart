522ec0b1eec0ff4e9b87196d68ecffe6c39e3eec
JEON-Sungsu <anagma@naver.com>
2024-05-10 18:14:39 +0900

    refactor: 위치 권한 요청 수정
    - 권한 확인 후 다음페이지로 이동하도록 변경

--- lib/common/presentation/splash_screen.dart
+++ lib/common/presentation/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:leave_subway/common/const/color.dart';
+import 'package:leave_subway/home/presentation/destination_set_screen.dart';
 import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,6 +12,8 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late bool _serviceEnabled;
+  late LocationPermission _permission;
 
   @override
   void initState() {
@@ -38,26 +41,28 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+    _serviceEnabled =
+        await Geolocator.isLocationServiceEnabled(); //위치정보 서비스가 이용가능한 기기인지 체크
 
-    serviceEnabled =
-    await Geolocator.isLocationServiceEnabled(); //위치정보 서비스가 이용가능한 기기인지 체크
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
+    _permission = await Geolocator.checkPermission(); //권한 획득했는지 확인
+    if (_permission == LocationPermission.denied) {
+      _permission = await Geolocator.requestPermission(); //권한 요청하기
     }
 
-    permission = await Geolocator.checkPermission(); //권한 획득했는지 확인
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission(); //권한 요청하기
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
+    if (_permission == LocationPermission.deniedForever) {
+      _permission = LocationPermission.deniedForever;
     }
 
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
+    Future.delayed(const Duration(seconds: 1), () {
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (_) => DestinationSetScreen(
+            serviceEnabled: _serviceEnabled,
+            permission: _permission,
+          ),
+        ),
+            (route) => false,
+      );
+    });
   }
 }

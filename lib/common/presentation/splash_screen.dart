4cf3c68060e1faad1dcb80ed73de563d6a189f45
JEON-Sungsu <anagma@naver.com>
2024-05-13 20:53:11 +0900

    feat: notification 권한 요청

--- lib/common/presentation/splash_screen.dart
+++ lib/common/presentation/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:leave_subway/common/const/color.dart';
+import 'package:leave_subway/core/notification/local_notification_setting.dart';
 import 'package:leave_subway/home/presentation/destination_set_screen.dart';
 import 'package:lottie/lottie.dart';
 
@@ -12,13 +13,26 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  late bool _serviceEnabled;
-  late LocationPermission _permission;
+  late bool _locationServiceEnabled;
+  late LocationPermission _locationPermission;
 
   @override
   void initState() {
     super.initState();
     _determinePosition();
+    initLocalNotification();
+
+    // Future.delayed(const Duration(seconds: 3), () {
+    //   Navigator.of(context).pushAndRemoveUntil(
+    //     MaterialPageRoute(
+    //       builder: (_) => DestinationSetScreen(
+    //         locationServiceEnabled: _locationServiceEnabled,
+    //         locationPermission: _locationPermission,
+    //       ),
+    //     ),
+    //         (route) => false,
+    //   );
+    // });
   }
 
   @override
@@ -32,7 +46,7 @@ class _SplashScreenState extends State<SplashScreen> {
           child: CircleAvatar(
             radius: 100,
             backgroundColor: Colors.white,
-            child: Lottie.asset('asset/json/splash_temp.json',
+            child: Lottie.asset('asset/json/splash_metro.json',
                 width: 200, height: 200),
           ),
         ),
@@ -41,28 +55,16 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> _determinePosition() async {
-    _serviceEnabled =
+    _locationServiceEnabled =
         await Geolocator.isLocationServiceEnabled(); //위치정보 서비스가 이용가능한 기기인지 체크
 
-    _permission = await Geolocator.checkPermission(); //권한 획득했는지 확인
-    if (_permission == LocationPermission.denied) {
-      _permission = await Geolocator.requestPermission(); //권한 요청하기
+    _locationPermission = await Geolocator.checkPermission(); //권한 획득했는지 확인
+    if (_locationPermission == LocationPermission.denied) {
+      _locationPermission = await Geolocator.requestPermission(); //권한 요청하기
     }
 
-    if (_permission == LocationPermission.deniedForever) {
-      _permission = LocationPermission.deniedForever;
+    if (_locationPermission == LocationPermission.deniedForever) {
+      _locationPermission = LocationPermission.deniedForever;
     }
-
-    Future.delayed(const Duration(seconds: 1), () {
-      Navigator.of(context).pushAndRemoveUntil(
-        MaterialPageRoute(
-          builder: (_) => DestinationSetScreen(
-            serviceEnabled: _serviceEnabled,
-            permission: _permission,
-          ),
-        ),
-            (route) => false,
-      );
-    });
   }
 }

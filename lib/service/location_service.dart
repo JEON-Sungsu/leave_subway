3020f69eda4e7595a48c07a456f84d5c817377be
JEON-Sungsu <anagma@naver.com>
2024-06-04 15:44:52 +0900

    feat: 위치 추적 기능 추가
    - riverpod 상태관리 추가
    - 구독 시작 기능 추가
    - 구독 취소 기능 추가

--- lib/service/location_service.dart
+++ lib/service/location_service.dart
@@ -1,16 +1,61 @@
 import 'dart:async';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:leave_subway/common/model/location_service_model.dart';
 import 'package:leave_subway/service/service_config.dart';
 
-StreamSubscription<Position> getStartLocationSubscription(
-    double destinationLat, double destinationLon) {
-  final subscription =
-      Geolocator.getPositionStream(locationSettings: LOCATION_SETTING).listen(
-    (Position? position) {
-      print('위치추적 시작');
-    },
-  );
+final locationServiceProvider =
+    StateNotifierProvider<LocationServiceNotifier, LocationServiceModel>((ref) {
+  return LocationServiceNotifier();
+});
 
-  return subscription;
+class LocationServiceNotifier extends StateNotifier<LocationServiceModel> {
+  LocationServiceNotifier() : super(LocationServiceModel());
+
+  void getStartLocationSubscription(
+      double destinationLat, double destinationLon) {
+    final subscription =
+        Geolocator.getPositionStream(locationSettings: LOCATION_SETTING).listen(
+      (Position? position) {
+        if (position != null) {
+          final distanceInMeters = Geolocator.distanceBetween(position.latitude,
+              position.longitude, destinationLat, destinationLon);
+          print(distanceInMeters);
+
+          state = state.copyWith(
+              distanceInMeters: distanceInMeters);
+        }
+
+        if (state.distanceInMeters != null) {
+          switch (state.distanceInMeters!) {
+            case <= 500:
+              print('목적지 500미터 내외 입니다. 내릴 준비를 해주세요. ');
+              cancelLocationSubscription();
+              state = state.copyWith(isCancel: true);
+              return;
+            case <= 1000:
+              print('약 3분 내외로 도착할 예정입니다.');
+          }
+        }
+      },
+    );
+
+    state = state.copyWith(
+        subscription: subscription,
+        isCancel: false
+    );
+  }
+
+  void cancelLocationSubscription() {
+    final subscription = state.subscription;
+
+    if (subscription == null) {
+      return;
+    }
+    subscription.cancel();
+
+    state = state.copyWith(subscription: subscription);
+    print('구독 취소');
+  }
 }

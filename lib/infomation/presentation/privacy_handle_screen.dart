dc9438030a2a6771134e078b86db115b369606cb
JEON-Sungsu <anagma@naver.com>
2024-06-21 22:45:20 +0900

    feat: 개인정보처리방침 웹뷰 화면 추가

--- lib/infomation/presentation/privacy_handle_screen.dart
+++ lib/infomation/presentation/privacy_handle_screen.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:leave_subway/common/presentation/default_layout.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
-class PrivacyHandleScreen extends StatelessWidget {
-  const PrivacyHandleScreen({super.key});
+class PrivacyHandleScreen extends StatefulWidget {
+  PrivacyHandleScreen({super.key});
+
+  @override
+  State<PrivacyHandleScreen> createState() => _PrivacyHandleScreenState();
+}
+
+class _PrivacyHandleScreenState extends State<PrivacyHandleScreen> {
+  late final _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(
+          'https://www.termsfeed.com/live/fee9960b-f97d-4fc5-981f-697eb07b951d'));
+
+
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+
+    _controller = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '개인정보 처리 방침',
-      child: Center(
-        child: Text('HIHI'),
+      child: WebViewWidget(
+        controller: _controller,
       ),
     );
   }
